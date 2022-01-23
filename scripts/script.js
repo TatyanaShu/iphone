c6c04717a01fb8838854c2da9c90bf2ee6bce70a
Tatyana Shuniborova <tashu529@gmail.com>
2022-01-23 22:12:08 +0300

    feat

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,37 @@
+//smooth scroll  page
 const links = document.querySelectorAll('.header-menu__item a');
+const addLink = document.querySelector('.card-details__link-characteristics');
+let arr = [];
+for (let i = 0; i < links.length; i++) {
+    arr.push(links[i]);
+}
+arr.push(addLink);
+seamless.polyfill();
 
-links.forEach((element)=> {
-    element.onclick = () => {
-        console.log('click')
-    }
+arr.forEach((element)=> {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const id = element.getAttribute('href').substring(1);
+        const section = document.getElementById(id);
+        // console.log('section', section)
+        console.log( id);
+       
+        if (section) {
+            seamless.elementScrollIntoView(section, {
+                behavior: 'smooth',
+                block: 'start'
+            });
+
+            console.log('this link consist address');
+        } else {
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+            console.log("element doesn't consist address");
+        }
+    })
 })
-
  
